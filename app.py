5fa1469754b6a2a0dded1c89a39fa89c0480d6f9
Jakub Chmiel <jakub.chmiel@roche.com>
2017-10-05 19:11:51 +0200

    test webhook

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 from flask import Flask,  make_response, request, render_template
 import sys
-
+import json
 
 app = Flask(__name__)
 
@@ -8,6 +9,30 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     return "<h1>Hello heroku</h1><p>Python: {ver}</p>".format(ver=sys.version)
+
+
+@app.route('/webhook')
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    req_dump = json.dumps(req, indent=4)
+    print('Request:\n{0}'.format(req_dump))
+
+    speech = 'gwóźdź'
+    response = {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "mr-muscle-bot"
+    }
+
+    response = json.dumps(response, indent=4)
+    print('Response:\n{0}'.format(response))
+
+    r = make_response(response)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
